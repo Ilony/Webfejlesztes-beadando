4943e6a4b7a02ce9420b9fd0bd513917d9cf9704
Ilony <139375513+Ilony@users.noreply.github.com>
2025-04-13 11:33:39 +0200

    Update bus-list.component.ts

--- src/app/components/bus-list/bus-list.component.ts
+++ src/app/components/bus-list/bus-list.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';  // Importáljuk a CommonModule-t
+import { CommonModule } from '@angular/common';  
 
 @Component({
   selector: 'app-bus-list',
   standalone: true,
   templateUrl: './bus-list.component.html',
   styleUrls: ['./bus-list.component.css'],
-  imports: [CommonModule]  // Add hozzá a CommonModule-t
+  imports: [CommonModule]  
 })
 export class BusListComponent {
   @Input() buses: Array<{ id: number; busNumber: string; origin: string; destination: string; departureTime: string }> = [];
