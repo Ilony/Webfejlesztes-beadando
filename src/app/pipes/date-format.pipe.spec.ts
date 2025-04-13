993553ea9c3b77140989d82b433c8eefae8ada79
Ilony <139375513+Ilony@users.noreply.github.com>
2025-04-13 11:32:31 +0200

    Update date-format.pipe.spec.ts

--- src/app/pipes/date-format.pipe.spec.ts
+++ src/app/pipes/date-format.pipe.spec.ts
@@ -1,18 +1,18 @@
 import { DateFormatPipe } from './date-format.pipe';
-import { DatePipe } from '@angular/common';  // Ne felejtsd el importálni a DatePipe-ot
+import { DatePipe } from '@angular/common';  
 
 describe('DateFormatPipe', () => {
   let pipe: DateFormatPipe;
   let datePipe: DatePipe;
 
   beforeEach(() => {
-    datePipe = new DatePipe('en-US'); // Példányosítjuk a DatePipe-ot
-    pipe = new DateFormatPipe(datePipe);  // Átadjuk a DatePipe-ot a DateFormatPipe konstruktorának
+    datePipe = new DatePipe('en-US'); 
+    pipe = new DateFormatPipe(datePipe);  
   });
 
   it('create an instance', () => {
     expect(pipe).toBeTruthy();
   });
 
-  // További tesztek hozzáadása szükséges
+
 });
